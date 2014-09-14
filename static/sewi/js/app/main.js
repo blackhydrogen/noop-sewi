8828148a6ceb310561ea9d91d7c973c57c300b59
Ian Leow <ianleow.nus@gmail.com>
2014-09-14 23:44:08 +0800

    Defined sewi.inherit function for JS Inheritance!

--- static/sewi/js/app/main.js
+++ static/sewi/js/app/main.js
@@ -1,4 +1,12 @@
 // Main program logic goes here.
+var sewi = sewi || {};
+
+sewi.inherit = function(obj) {
+    var F = function () {};
+    F.prototype = obj;
+    return new F();
+};
+
 (function() {
 
 })();
