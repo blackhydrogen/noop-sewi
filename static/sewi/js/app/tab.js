e35f1105305524eecc6214baa64cf61ee478d055
Le Beier <moxlotus@live.com>
2014-09-15 14:24:01 +0800

    Tab Container Update 1. Added simple Droppable functionality 2. Show droppable area upon draggable is moved.

--- static/sewi/js/app/tab.js
+++ static/sewi/js/app/tab.js
@@ -7,49 +7,21 @@ sewi.TabContainer = function(){
 	selfRef.currentActiveTabButton;
 	selfRef.tabButtons=[];
 	selfRef.tabContentPanels=[];	
-	
+	selfRef.resourceList=[];
+
 	selfRef.container = $('<div class="tab-container"></div>');
 	selfRef.tabButtonGroup = $('<ul id="tab-button-group" class="nav nav-tabs" role="tablist"></ul>');
 	selfRef.tabContent = $('<div class="tab-content"></div>');
 	
-	
-	var addTabButton = $('<li><a class="add-tab-button"><span class="glyphicon glyphicon-plus"></span></a></li>');
-	addTabButton.on('click', function(){
-		selfRef.addNewTab("Tab"+counter,"Tab "+counter,true);
-		counter++;
-	});
-
-	selfRef.tabButtons.push(addTabButton);
-	selfRef.tabButtonGroup.append(addTabButton);
-	
-	selfRef.addNewTab("tab"+counter, "Tab "+counter, true);
-	counter++;
-		
-	selfRef.container.append(selfRef.tabButtonGroup);
-	selfRef.container.append(selfRef.tabContent);
-}
-
-sewi.TabContainer.prototype.addNewTab = function(tabName, tabText, active){
-	var selfRef = this;
-	var buttonDOM=$('<li><a href="#'+tabName+'" role="tab" data-toggle="tab">'+tabText+'</a></li>');
-	var panelDOM=$('<div class="tab-pane" id="'+tabName+'">'+tabText+'</div>');
-	var removeButton={ tabButton : buttonDOM,
-						tabPanel : panelDOM,
-						  	 DOM : $('<span class="glyphicon glyphicon-remove"></span>')};
-	if(active){
-		selfRef.setCurrentActiveTab(panelDOM); 
-		selfRef.setCurrentActiveTabButton(buttonDOM);
-	}
-	
-	buttonDOM.children('a').append(removeButton.DOM);
-	
-	removeButton.DOM.on('click', function(){
+	selfRef.removeEvent = function(event){
+		var removeButton = event.data;
+		console.log(event.data);
 		var buttonIndex = selfRef.tabButtons.indexOf(removeButton.tabButton);
 		var panelIndex = selfRef.tabContentPanels.indexOf(removeButton.tabPanel);
 		
 		removeButton.tabPanel.remove();
 		removeButton.tabButton.remove();
-
+		
 		selfRef.tabButtons.splice(buttonIndex, 1);
 		selfRef.tabContentPanels.splice(panelIndex, 1);
 	
@@ -66,13 +38,58 @@ sewi.TabContainer.prototype.addNewTab = function(tabName, tabText, active){
 		if(selfRef.tabButtons.length == 1){
 			selfRef.container.trigger("NoTabs");
 		}
+	}
+
+	selfRef.tabButtonEvent = function(event){
+		selfRef.setCurrentActiveTab(event.data.panelDOM);
+		selfRef.setCurrentActiveTabButton(event.data.buttonDOM);
+	}
+
+	var addTabButton = $('<li><a class="add-tab-button"><span class="glyphicon glyphicon-plus"></span></a></li>');
+	addTabButton.on('click', function(){
+		selfRef.addNewTab("Tab"+counter,"Tab "+counter,true);
+		counter++;
 	});
 
-	buttonDOM.on('click', function(){
-		selfRef.setCurrentActiveTab(panelDOM);
-		selfRef.setCurrentActiveTabButton(buttonDOM);
-	});
+	selfRef.tabButtons.push(addTabButton);
+	selfRef.tabButtonGroup.append(addTabButton);
+			
+	selfRef.container.append(selfRef.tabButtonGroup);
+	selfRef.container.append(selfRef.tabContent);
+}
+
+sewi.TabContainer.prototype.addNewTab = function(tabName, tabText, active){
+	var selfRef = this;
+	var buttonDOM=$('<li><a href="#'+tabName+'" role="tab" data-toggle="tab">'+tabText+'</a></li>');
+	var dropAreaDOM = $('<div class="panel-drop-area"></div>');
+	var panelDOM=$('<div class="tab-pane" id="'+tabName+'"><div class="panel-content">'+tabText+'</div></div>');
+	var removeButton={ tabButton : buttonDOM,
+			tabPanel : panelDOM,
+				DOM : $('<span class="glyphicon glyphicon-remove"></span>')};
 	
+	
+	panelDOM.prepend(dropAreaDOM);
+
+	if(active){
+		selfRef.setCurrentActiveTab(panelDOM); 
+		selfRef.setCurrentActiveTabButton(buttonDOM);
+	}
+	
+	dropAreaDOM.droppable({
+			drop : function(event, ui){
+				// TO DO: initialize the component based on the type and id.
+				console.log(ui.draggable);
+			},
+			over : function(){
+				console.log("HOVER!!!");
+			},
+			activeClass : 'panel-drop-area-visible'
+	});
+
+	buttonDOM.children('a').append(removeButton.DOM);	
+	removeButton.DOM.on('click', removeButton, selfRef.removeEvent);
+	buttonDOM.on('click',{ buttonDOM : buttonDOM, panelDOM : panelDOM }, selfRef.tabButtonEvents);
+
 	var lastIndex = selfRef.tabButtons.length-1;
 	buttonDOM.insertBefore(selfRef.tabButtons[lastIndex]);
 	selfRef.tabButtons.splice(lastIndex, 0, buttonDOM);
