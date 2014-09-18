1d8025cb5c752dd4c82082314dff00df6b8b23c8
Le Beier <moxlotus@live.com>
2014-09-18 21:28:29 +0800

    Tab Container Update 1. code refracted 2. Methods are being abstracted to a new class called Tab

--- static/sewi/js/app/main.js
+++ static/sewi/js/app/main.js
@@ -1,10 +1,26 @@
 // Main program logic goes here.
 $(function() {
-    var configurator = new sewi.Configurator({
-        basicInfoView: '#beiView',
-        resViewerView: '#mainView',
-        resExplorerView: '#resView',
+
+  var configurator = new sewi.Configurator({
+      basicInfoView: '#beiView',
+      resViewerView: '#mainView',
+      resExplorerView: '#resView',
         
-        isResourceViewerHidden: false
-    });
+      isResourceViewerHidden: false
+  });
+
+  var tempDrag = $('<div id="draggable"></div>');
+  tempDrag.css('background-color', 'red');
+  tempDrag.css('width', '100px');
+  tempDrag.css('height', '100px');
+  tempDrag.draggable({revert : 'invalid', helper:'clone'});
+  $('#resView').append(tempDrag);
+  
+  var tempDrag2 = $('<div id="draggable"></div>');
+  tempDrag2.css('background-color', 'red');
+  tempDrag2.css('width', '100px');
+  tempDrag2.css('height', '100px');
+  tempDrag2.draggable({revert : 'invalid', helper:'clone'});
+  $('#resView').append(tempDrag2);
+
 });
--- static/sewi/js/app/tab.js
+++ static/sewi/js/app/tab.js
@@ -1,49 +1,155 @@
 var sewi = sewi || {}; 
+
+// Tab class
+sewi.Tab = function(tabContainer, id, name){
+	var selfRef = this;
+	selfRef.DROP_AREA_HOVER = 'panel-drop-area-hover';
+	
+	var dropAreaDOM = {
+			Top : $('<div class="panel-drop-area panel-drop-area-top"></div>'),
+			Bottom : $('<div class="panel-drop-area panel-drop-area-bottom"></div>'),
+			Left : $('<div class="panel-drop-area panel-drop-area-left"></div>'),
+			Right : $('<div class="panel-drop-area panel-drop-area-right"></div>')
+		};
+
+	selfRef.tabContainer = tabContainer;
+	selfRef.tabButton = $('<li><a href="#'+id+'" role="tab" data-toggle="tab">'+name+'</a></li>');
+	selfRef.tabPanel = $('<div class="tab-pane" id="'+id+'"><div class="panel-content">'+name+'</div></div>');
+	
+	var removeButton={  	tabRef : selfRef,
+				containerRef : tabContainer,
+				tabButton : selfRef.tabButton,
+				tabPanel : selfRef.tabPanel,
+				      DOM : $('<span class="glyphicon glyphicon-remove"></span>')};
+	
+	selfRef.tabButtonEvent = function(){
+		selfRef.tabContainer.setCurrentActiveTab(selfRef);
+	}	
+
+	selfRef.tabPanel.append(dropAreaDOM.Top);
+        selfRef.tabPanel.append(dropAreaDOM.Bottom);
+	selfRef.tabPanel.append(dropAreaDOM.Left);
+	selfRef.tabPanel.append(dropAreaDOM.Right);
+	
+	selfRef.setDroppable(dropAreaDOM.Top);
+	selfRef.setDroppable(dropAreaDOM.Bottom);
+	selfRef.setDroppable(dropAreaDOM.Left);
+	selfRef.setDroppable(dropAreaDOM.Right);	
+
+	removeButton.DOM.on('click', removeButton, selfRef.removeEvent);
+	selfRef.tabButton.children('a').append(removeButton.DOM);
+	selfRef.tabButton.on('click', selfRef.tabButtonEvent);
+}
+
+sewi.Tab.prototype.removeEvent = function(event){
+	var removeButton = event.data;
+	var selfRef = removeButton.tabRef;
+	var containerRef = removeButton.containerRef;
+	var tabIndex = containerRef.tabs.indexOf(selfRef);
+		
+	removeButton.tabPanel.remove();
+	removeButton.tabButton.remove();
+
+	containerRef.tabs.splice(tabIndex, 1);
+
+	if(containerRef.currentActiveTab == selfRef){
+		console.log("active");
+		if(tabIndex == containerRef.tabs.length){
+			containerRef.setCurrentActiveTab(containerRef.tabs[tabIndex-1]);
+		} else {
+			containerRef.setCurrentActiveTab(containerRef.tabs[tabIndex]);
+		}
+	} 
+
+	if(containerRef.tabs.length == 0){
+		selfRef.container.trigger("NoTabs");
+	}
+}
+
+sewi.Tab.prototype.append = function(DOMObject){
+	var selfRef = this;
+	if(selfRef.isHover('.panel-drop-area-left')){
+		console.log('left is active');
+		if(selfRef.isHover('.panel-drop-area-top')){
+			console.log('top is active');
+		} else if(selfRef.isHover('.panel-drop-area-bottom')){
+			console.log('bottom is active');
+		}
+	}
+	
+	if(selfRef.isHover('.panel-drop-area-right')){
+		console.log('right is active');
+		if(selfRef.isHover('.panel-drop-area-top')){
+			console.log('top is active');
+		} else if(selfRef.isHover('.panel-drop-area-bottom')){
+			console.log('bottom is active');
+		}
+	}
+	selfRef.tabPanel.append(DOMObject);
+}
+
+sewi.Tab.prototype.setDroppable = function(DOMObject){
+	var selfRef = this;
+	DOMObject.droppable({
+		drop: function(event, ui){
+					console.log(ui.draggable);
+					selfRef.addResource(ui.draggable);
+				},
+		over: function(){	
+		 },
+		activeClass: 'panel-drop-area-visible',
+		hoverClass: selfRef.DROP_AREA_HOVER
+	});
+}
+
+sewi.Tab.prototype.activate = function(){
+	var selfRef = this;	
+	selfRef.tabButton.addClass('active');
+	selfRef.tabPanel.addClass('active');
+}
+
+sewi.Tab.prototype.deactivate = function(){
+	var selfRef = this;
+	selfRef.tabButton.removeClass('active');
+	selfRef.tabPanel.removeClass('active');
+}
+
+sewi.Tab.prototype.isHover = function(dropArea){
+	var selfRef = this;
+	return selfRef.tabPanel.children(dropArea).hasClass(selfRef.DROP_AREA_HOVER);
+}
+
+sewi.Tab.prototype.setDroppable = function(DOMObject){
+	var selfRef = this;
+	DOMObject.droppable({
+		drop: function(event, ui){
+			console.log(ui.draggable);
+			selfRef.append(ui.draggable);
+		},
+		over: function(){	
+		 },
+		activeClass: 'panel-drop-area-visible',
+		hoverClass: selfRef.DROP_AREA_HOVER
+	});
+}
+
+// tab container class
 sewi.TabContainer = function(){
 	var selfRef = this;
 	var counter= 1;
 	
+	//Static String values
+	selfRef.DROP_AREA_HOVER = 'panel-drop-area-hover';
+
 	selfRef.currentActiveTab;
-	selfRef.currentActiveTabButton;
-	selfRef.tabButtons=[];
-	selfRef.tabContentPanels=[];	
+	selfRef.tabButtons=[];	
 	selfRef.resourceList=[];
+
+	selfRef.tabs = [];
 
 	selfRef.container = $('<div class="tab-container"></div>');
 	selfRef.tabButtonGroup = $('<ul id="tab-button-group" class="nav nav-tabs" role="tablist"></ul>');
 	selfRef.tabContent = $('<div class="tab-content"></div>');
-	
-	selfRef.removeEvent = function(event){
-		var removeButton = event.data;
-		console.log(event.data);
-		var buttonIndex = selfRef.tabButtons.indexOf(removeButton.tabButton);
-		var panelIndex = selfRef.tabContentPanels.indexOf(removeButton.tabPanel);
-		
-		removeButton.tabPanel.remove();
-		removeButton.tabButton.remove();
-		
-		selfRef.tabButtons.splice(buttonIndex, 1);
-		selfRef.tabContentPanels.splice(panelIndex, 1);
-	
-		if(selfRef.currentActiveTabButton == removeButton.tabButton){
-			if(buttonIndex==selfRef.tabButtons.length-1){
-				selfRef.setCurrentActiveTab(selfRef.tabContentPanels[panelIndex-1]);
-				selfRef.setCurrentActiveTabButton(selfRef.tabButtons[buttonIndex-1]);
-			} else {
-				selfRef.setCurrentActiveTab(selfRef.tabContentPanels[panelIndex]);
-				selfRef.setCurrentActiveTabButton(selfRef.tabButtons[buttonIndex]);
-			}
-		} 
-
-		if(selfRef.tabButtons.length == 1){
-			selfRef.container.trigger("NoTabs");
-		}
-	}
-
-	selfRef.tabButtonEvent = function(event){
-		selfRef.setCurrentActiveTab(event.data.panelDOM);
-		selfRef.setCurrentActiveTabButton(event.data.buttonDOM);
-	}
 
 	var addTabButton = $('<li><a class="add-tab-button"><span class="glyphicon glyphicon-plus"></span></a></li>');
 	addTabButton.on('click', function(){
@@ -56,76 +162,30 @@ sewi.TabContainer = function(){
 			
 	selfRef.container.append(selfRef.tabButtonGroup);
 	selfRef.container.append(selfRef.tabContent);
+
 }
 
 sewi.TabContainer.prototype.addNewTab = function(tabName, tabText, active){
 	var selfRef = this;
-	var buttonDOM=$('<li><a href="#'+tabName+'" role="tab" data-toggle="tab">'+tabText+'</a></li>');
-	var dropAreaDOM = $('<div class="panel-drop-area"></div>');
-	var panelDOM=$('<div class="tab-pane" id="'+tabName+'"><div class="panel-content">'+tabText+'</div></div>');
-	var removeButton={ tabButton : buttonDOM,
-			tabPanel : panelDOM,
-				DOM : $('<span class="glyphicon glyphicon-remove"></span>')};
-	
-	
-	panelDOM.prepend(dropAreaDOM);
+	var newTab = new sewi.Tab(selfRef, tabName, tabText);
+	selfRef.tabs.push(newTab);
 
-	if(active){
-		selfRef.setCurrentActiveTab(panelDOM); 
-		selfRef.setCurrentActiveTabButton(buttonDOM);
-	}
+	if(active) selfRef.setCurrentActiveTab(newTab);	
 	
-	dropAreaDOM.droppable({
-			drop : function(event, ui){
-				// TO DO: initialize the component based on the type and id.
-				console.log(ui.draggable);
-			},
-			over : function(){
-				console.log("HOVER!!!");
-			},
-			activeClass : 'panel-drop-area-visible'
-	});
-
-	buttonDOM.children('a').append(removeButton.DOM);	
-	removeButton.DOM.on('click', removeButton, selfRef.removeEvent);
-	buttonDOM.on('click',{ buttonDOM : buttonDOM, panelDOM : panelDOM }, selfRef.tabButtonEvents);
-
-	var lastIndex = selfRef.tabButtons.length-1;
-	buttonDOM.insertBefore(selfRef.tabButtons[lastIndex]);
-	selfRef.tabButtons.splice(lastIndex, 0, buttonDOM);
-	selfRef.tabContentPanels.push(panelDOM);
-	selfRef.tabContent.append(panelDOM);
+	var lastIndex = selfRef.tabButtons.length-1;	
+	newTab.tabButton.insertBefore(selfRef.tabButtons[lastIndex]);
+	selfRef.tabContent.append(newTab.tabPanel);
 }
 
-sewi.TabContainer.prototype.setCurrentActiveTab = function(DOMObject){
-	var selfRef = this;
+sewi.TabContainer.prototype.setCurrentActiveTab = function(tab){
+	var selfRef = this;	
 	if(selfRef.currentActiveTab){
-		selfRef.currentActiveTab.removeClass('active');
+		selfRef.currentActiveTab.deactivate();
 	}
-	selfRef.currentActiveTab = DOMObject;
-	if(DOMObject){
-		selfRef.currentActiveTab.addClass('active');
+	selfRef.currentActiveTab = tab;
+	if(tab){
+		selfRef.currentActiveTab.activate();
 	}
-}
-
-sewi.TabContainer.prototype.setCurrentActiveTabButton = function(DOMObject){
-	var selfRef = this;
-	if(selfRef.currentActiveTabButton){
-		selfRef.currentActiveTabButton.removeClass('active');
-	}
-	selfRef.currentActiveTabButton = DOMObject;
-	if(DOMObject){
-		selfRef.currentActiveTabButton.addClass('active');
-	}
-}
-
-sewi.TabContainer.prototype.addResource = function(DOMObject){
-	var selfRef = this;
-	selfRef.currentActiveTab.append(DOMObject);
-}
-
-sewi.TabContainer.prototype.update = function(){
-	var selfRef = this;
 }
 
 sewi.TabContainer.prototype.getDOM = function(){
