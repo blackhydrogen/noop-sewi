4c3314848301ffa4515e3845c6524d32805cd71d
Ian Leow <ianleow.nus@gmail.com>
2014-09-23 10:51:14 +0800

    Migrated image resource standalone to actual scripts.

--- static/sewi/js/app/imageResourceViewer.js
+++ static/sewi/js/app/imageResourceViewer.js
@@ -7,37 +7,249 @@ sewi.ImageResourceViewer = function(options) {
 
 	// TODO $.getJSON("json/image.json", function( imageURL ) {
 
-	selfRef.mainContainer = $("<div class='image-resource-container'><div class='image-resource-image-container'><img src='sf.jpg' class='image-resource-image'></div><div class='image-resource-control-panel animated'><div class='image-resource-control'>Brightness<br><input type='range' class='image-resource-brightness' min='0' max='2' step='0.1'></div><div class='image-resource-control'>Contrast<br><input type='range' class='image-resource-contrast' min='0' max='2' step='0.1'></div><div class='image-resource-control'><label><input type='checkbox' class='image-resource-invert'>Invert</label></div><div class='image-resource-control'><input type='button' value='Reset' class='image-resource-reset-button'></div></div><input type='button' value='Hide' class='image-resource-show-hide-button'></div>");
+	selfRef.mainContainer = $("<div class='image-resource-container'></div>");
+		var imageContainer = $("<div class='image-resource-image-container'></div>")
+		.appendTo(mainContainer);
+			var imageElement = $("<img src='sf.jpg' class='image-resource-image'>")
+			.appendTo(imageContainer);
+		var controlsContainer = $("<div class='image-resource-control-panel animated'></div>")
+		.appendTo(mainContainer);
+			var brightnessControlContainer = $("<div class='image-resource-control'></div>")
+			.appendTo(controlsContainer)
+			.html("Brightness<br>");
+				var brightnessControl = $("<input type='range' class='image-resource-brightness' min='0' max='2' step='0.1'>")
+				.appendTo(brightnessControlContainer);
+			var contrastControlContainer = $("<div class='image-resource-control'></div>")
+			.appendTo(controlsContainer)
+			.html("Contrast<br>");
+				var contrastControl = $("<input type='range' class='image-resource-contrast' min='0' max='2' step='0.1'>")
+				.appendTo(contrastControlContainer);
+			var invertControlContainer = $("<div class='image-resource-control'></div>")
+			.appendTo(controlsContainer)
+				var invertControlLabel = $("<label>Invert</label>")
+				.appendTo(invertControlContainer);
+					var invertControl = $("<input type='checkbox' class='image-resource-invert'>")
+					.prependTo(invertControlLabel);
+			var zoomControlContainer = $("<div class='image-resource-control'></div>")
+			.appendTo(controlsContainer)
+			.html("Zoom<br>");
+				var zoomControl = $("<input type='number' class='image-resource-zoom' min='10' max='200' step='10'>")
+				.appendTo(zoomControlContainer);
+				$("<span>%</span>")
+				.appendTo(zoomControlContainer)
+				var zoom100PercentButton = $("<input type='button' class='image-resource-zoom-100' value='100%'>")
+				.appendTo(zoomControlContainer);
+				var zoomFitButton = $("<input type='button' class='image-resource-zoom-fit' value='Fit'>")
+				.appendTo(zoomControlContainer);
+			var resetControlContainer = $("<div class='image-resource-control'></div>")
+			.appendTo(controlsContainer);
+				var resetControl = $("<input type='button' value='Reset' class='image-resource-reset-button'>")
+				.appendTo(resetControlContainer);
+		var showHideControlsButton = $("<input type='button' value='Hide Controls' class='image-resource-show-hide-button'>")
+		.appendTo(mainContainer);
 
-	$(".image-resource-brightness").on("input", adjustImageFilters);
-	$(".image-resource-contrast").on("input", adjustImageFilters);
-	$(".image-resource-invert").on("change", adjustImageFilters);
+	var imageSizeProperties = {
+		original: {},
+		fit: {},
+		minZoom: {},
+		maxZoom: {}
+	};
 
-	$(".image-resource-reset-button").on("click", function() {
-		$(".image-resource-brightness").val(1);
-		$(".image-resource-contrast").val(1);
-		$(".image-resource-invert").prop("checked", false);
-		adjustImageFilters();
-	})
+	var imageMovementVariables = {};
 
-	$(".image-resource-show-hide-button").on("click", function() {
-		if($(this).val() == "Hide") {
-			$(".image-resource-control-panel").addClass("image-resource-control-panel-hidden");
-			$(this).val("Show");
+	function moveImageToCursor(event) {
+		if(imageMovementVariables.ignoreEvent) return false;
+		var newImageX =  imageMovementVariables.originalImageX + event.pageX - imageMovementVariables.originalCursorX;
+		var newImageY =  imageMovementVariables.originalImageY + event.pageY - imageMovementVariables.originalCursorY;
+
+		if(newImageX < imageMovementVariables.minImageX)
+			newImageX = imageMovementVariables.minImageX;
+		else if(newImageX > imageMovementVariables.maxImageX)
+			newImageX = imageMovementVariables.maxImageX;
+
+		if(newImageY < imageMovementVariables.minImageY)
+			newImageY = imageMovementVariables.minImageY;
+		else if(newImageY > imageMovementVariables.maxImageY)
+			newImageY = imageMovementVariables.maxImageY;
+
+		imageElement.css({
+			top: newImageY,
+			left: newImageX
+		});
+
+		imageMovementVariables.ignoreEvent = true;
+		setTimeout(function() {
+			imageMovementVariables.ignoreEvent = false;
+		}, 10);
+
+		return false;
+	}
+
+	function calculateImageZoomFromMousewheel(event) {
+		var zoomChange = 1 + 0.2 * event.originalEvent.wheelDelta / 120;
+		var newImageWidth = imageElement.width() * zoomChange;
+
+		updateImageZoom(newImageWidth);
+
+		return false;
+	}
+
+	function calculateImageZoomFromZoomControl() {
+		var newImageWidth = imageSizeProperties.original.width * zoomControl.val() / 100;
+
+		updateImageZoom(newImageWidth);
+	}
+
+	function updateImageZoom(newImageWidth) {
+		if(newImageWidth < imageSizeProperties.minZoom.width) {
+			newImageWidth = imageSizeProperties.minZoom.width;
+		}
+		else if(newImageWidth > imageSizeProperties.maxZoom.width) {
+			newImageWidth = imageSizeProperties.maxZoom.width;
+		}
+
+		imageElement.width( newImageWidth );
+
+		if(newImageWidth < imageSizeProperties.fit.width) {
+			centreImageOnCanvas();
+		}
+
+		updateZoomPercentage();
+	}
+
+	function updateZoomPercentage() {
+		zoomControl.val( Math.floor(imageElement.width() / imageSizeProperties.original.width * 100) );
+	}
+
+	function setImageZoomToFit() {
+		imageElement.width( imageSizeProperties.fit.width );
+
+		updateZoomPercentage();
+
+		centreImageOnCanvas();
+	}
+
+	function centreImageOnCanvas() {
+		imageElement.css({
+			top: (imageContainer.height() - imageElement.height()) / 2,
+			left: (imageContainer.width() - imageElement.width()) / 2
+		});
+	}
+
+	function setImageSizeProperties() {
+
+		imageSizeProperties.original.width = imageElement.prop("naturalWidth");
+		imageSizeProperties.original.height = imageElement.prop("naturalHeight");
+
+		//Compute fit sizes
+		var originalWidth = imageElement.prop("naturalWidth");
+		var originalHeight = imageElement.prop("naturalHeight");
+		var containerWidth = imageContainer.width();
+		var containerHeight = imageContainer.height();
+
+		if(originalWidth <= containerWidth) {
+			if(originalHeight <= containerHeight) {
+				imageSizeProperties.fit.width = originalWidth;
+				imageSizeProperties.fit.height = originalHeight;
+			}
+			else {
+				imageSizeProperties.fit.width = originalWidth * containerHeight / originalHeight;
+				imageSizeProperties.fit.height = containerHeight;
+			}
 		}
 		else {
-			$(".image-resource-control-panel").removeClass("image-resource-control-panel-hidden");
-			$(this).val("Hide");	
+			if(originalHeight <= containerHeight) {
+				imageSizeProperties.fit.width = containerWidth;
+				imageSizeProperties.fit.height = originalHeight * containerWidth / originalWidth;
+			}
+			else {
+				var adjustedWidth = originalWidth * containerHeight / originalHeight;
+
+				if(adjustedWidth <= containerWidth) {
+					imageSizeProperties.fit.width = adjustedWidth;
+					imageSizeProperties.fit.height = originalHeight * adjustedWidth / originalWidth;
+				}
+				else {
+					imageSizeProperties.fit.width = containerWidth;
+					imageSizeProperties.fit.height = originalHeight * containerWidth / originalWidth;
+				}
+			}
+		}
+		// End Compute fit sizes
+
+		imageSizeProperties.minZoom.width = imageSizeProperties.fit.width / 2;
+		imageSizeProperties.minZoom.height = imageSizeProperties.fit.height / 2;
+
+		imageSizeProperties.maxZoom.width = imageSizeProperties.original.width * 2;
+		imageSizeProperties.maxZoom.height = imageSizeProperties.original.height * 2;
+	}
+
+	function adjustImageFilters() {
+		imageElement.css("-webkit-filter",
+				"brightness(" + brightnessControl.val() + ")"
+				+ "contrast(" + contrastControl.val() + ")"
+				+ "invert(" + (invertControl.prop("checked") ? 1 : 0) + ")");
+	}
+}
+
+sewi.ImageResourceViewer.prototype.load = function() {
+
+	brightnessControl.on("input", adjustImageFilters);
+	contrastControl.on("input", adjustImageFilters);
+	invertControl.on("change", adjustImageFilters);
+	zoomControl.on("change", calculateImageZoomFromZoomControl);
+	zoom100PercentButton.on("click", function() {
+		updateImageZoom(imageSizeProperties.original.width);
+	});
+	zoomFitButton.on("click", setImageZoomToFit);
+
+	resetControl.on("click", function() {
+		brightnessControl.val(1);
+		contrastControl.val(1);
+		invertControl.prop("checked", false);
+		adjustImageFilters();
+		setImageZoomToFit();
+	})
+
+	showHideControlsButton.on("click", function() {
+		if($(this).val() == "Hide Controls") {
+			controlsContainer.addClass("hidden");
+			$(this).val("Show Controls");
+		}
+		else {
+			controlsContainer.removeClass("hidden");
+			$(this).val("Hide Controls");	
 		}
 	});
 
-	function adjustImageFilters() {
-	$(".image-resource-image").css("-webkit-filter",
-			"brightness(" + $(".image-resource-brightness").val() + ")"
-			+ "contrast(" + $(".image-resource-contrast").val() + ")"
-			+ "invert(" + ($(".image-resource-invert").prop("checked") ? 1 : 0) + ")");
-	}
-}
+	imageContainer.on("mousewheel", calculateImageZoomFromMousewheel);
+
+	imageContainer.on("mousedown", function(event) {
+		imageMovementVariables.originalCursorX = event.pageX;
+		imageMovementVariables.originalCursorY = event.pageY;
+		imageMovementVariables.originalImageX = imageElement.position().left;
+		imageMovementVariables.originalImageY = imageElement.position().top;
+		imageMovementVariables.maxImageX = imageContainer.width() / 2;
+		imageMovementVariables.maxImageY = imageContainer.height() / 2;
+		imageMovementVariables.minImageX = imageMovementVariables.maxImageX - imageElement.width();
+		imageMovementVariables.minImageY = imageMovementVariables.maxImageY - imageElement.height();
+		imageMovementVariables.ignoreEvent = false; // Used for performances purposes. This variable is set when moveImageToCursor is called too quickly consecutively (after each call to moveImageToCursor, it is set to true, then a timeout() - with some delay - is set it back to false)
+
+		imageContainer.unbind("mousewheel", calculateImageZoomFromMousewheel); //Don't allow the user to move and zoom
+		$("body").on("mousemove", moveImageToCursor);
+	});
+
+	$("body").on("mouseup", function(event) {
+		imageContainer.on("mousewheel", calculateImageZoomFromMousewheel);
+		$("body").unbind("mousemove", moveImageToCursor);
+	});
+
+	imageElement.load(function() {
+		setImageSizeProperties();
+		setImageZoomToFit();
+	});
+	
+};
 
 // TODO Should be inherited from ConfiguratorElement
 sewi.ImageResourceViewer.prototype.getDOM = function() {
