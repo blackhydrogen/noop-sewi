902d1ac04f0b3df2c9cb308590281be010d43041
Bharath <bharathparam94@gmail.com>
2014-09-23 12:10:25 +0800

    added tooltip for showing meta data

--- static/sewi/js/app/resourceGallery.js
+++ static/sewi/js/app/resourceGallery.js
@@ -1,51 +1,64 @@
+var sewi = sewi || {};
+sewi.ResourceGallery = function() {
+    var selfRef = this;
 
-var sewi = sewi || {}; 
-sewi.ResourceGallery = function(){
-	var selfRef = this;
-	
-	selfRef.resources = [];
-	selfRef.resources.push("Sample Resources/hands.jpg", "Sample Resources/ecg.jpg", "Sample Resources/video.jpg");
-	selfRef .resourceHeaders = [];
-	selfRef.resourceHeaders.push("X-Ray-Stub", "ECG-Stub", "Video-Stub");
+    selfRef.resources = [];
+    selfRef.resources.push('Sample Resources/hands.jpg', 'Sample Resources/ecg.jpg', 'Sample Resources/video.jpg');
+    
+    selfRef.resourceHeaders = [];
+    selfRef.resourceHeaders.push('X-Ray-Stub', 'ECG-Stub', 'Video-Stub');
 
-	selfRef.container = $('<div>').addClass('resource-explorer-container').attr('id' ,"scrollable-div");	
-	selfRef.loadResources();
+    selfRef.metaData = [];
+    selfRef.metaData.push('24/11/2013');
+    
+    selfRef.container = $('<div>').addClass('resource-explorer-container').attr('id', 'scrollable-div');
+    selfRef.loadResources();
 
-		 $('#scrollable-div').slimScroll({
+    $('#scrollable-div').slimScroll({
         color: '#fff',
-        width: '72px', 
-        size: '4px', 
+        width: '72px',
+        size: '4px',
         height: '350px'
+    });
+
+    $("[rel='tooltip']").tooltip({
+        html: true
     });
 }
 
-sewi.ResourceGallery.prototype.loadResources = function(){
-		var selfRef = this;
-		for(var i=0;i<selfRef.resources.length;i++){
-			var path = selfRef.resources[i];
-		    var resourceElement = $('<div>')
-			.addClass('resource')
-			.attr("data-resId" ,i);			
-			//.draggable({ helper: 'clone'});
-			
-			resourceElement.on('dblclick', getResourceDom);
+sewi.ResourceGallery.prototype.loadResources = function() {
+    var selfRef = this;
+    for (var i = 0; i < selfRef.resources.length; i++) {
+        var path = selfRef.resources[i];
+        var resourceElement = $('<div>')
+            .addClass('resource')
+            .attr('data-resId', i)
+            .attr('data-resId', i)
+            .attr('rel', 'tooltip')
+            .attr('data-placement', 'left')
+            .attr('title', 'Recorded on:' + selfRef.metaData[0])
+        .draggable({
+            helper: 'clone'
+        });
 
-		resourceElement.append(
-			$('<img>').attr('src', path).addClass('media-thumbnail')
-		).append(
-			$('<p>').text(selfRef.resourceHeaders[i]).addClass('media-body')
-		);
+        resourceElement.on('dblclick', getResourceDom);
 
-		    
-		    selfRef.container.append(resourceElement);
-		}
+        resourceElement.append(
+            $('<img>').attr('src', path).addClass('media-thumbnail')
+        ).append(
+            $('<p>').text(selfRef.resourceHeaders[i]).addClass('media-body')
+        );
+
+
+        selfRef.container.append(resourceElement);
+    }
 
 }
 
-sewi.ResourceGallery.prototype.update = function(){
-	var selfRef = this;
+sewi.ResourceGallery.prototype.update = function() {
+    var selfRef = this;
 }
 
-sewi.ResourceGallery.prototype.getResourceDom = function(){
-	return($(this).attr('data-resId'));
-	}
+sewi.ResourceGallery.prototype.getResourceDom = function() {
+    return ($(this).attr('data-resId'));
+}
