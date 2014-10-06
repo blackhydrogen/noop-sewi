164f34d42ed676d2f21cdbba320863c4e3851aaa
TheFuzzy <Infestor51@gmail.com>
2014-10-06 11:54:38 +0800

    Added a inheritance helper function

--- static/sewi/js/app/common.js
+++ static/sewi/js/app/common.js
@@ -1,1 +1,14 @@
 var sewi = sewi || {};
+
+/**
+ * Helper function to ensure that a class inherits another class.
+ * @param  {function} subClass   Function definition of the sub class.
+ * @param  {function} superClass Function definition of the super class.
+ */
+sewi.inherits = function(subClass, superClass) {
+    if (_.isFunction(subClass) && _.isFunction(superClass)) {
+        subClass.prototype = _.create(superClass.prototype, {'constructor': subClass});
+    } else {
+        throw new Error('Only class definitions can inherit other class definitions.');
+    }
+}
