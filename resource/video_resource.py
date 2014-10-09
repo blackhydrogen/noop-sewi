eaf0bb4462167e59bbe320d406c310e08c0b166a
TheFuzzy <Infestor51@gmail.com>
2014-10-09 20:43:06 +0800

    Added logic to retrieve the observation and concept as a video resource

--- resource/video_resource.py
+++ resource/video_resource.py
@@ -1,25 +1,36 @@
 from . import BaseResource
+from mds.core.models import Observation
 
 class VideoResource(BaseResource):
     """A representation of a video Resource.
     """
 
     def __init__(self, resource_id):
-        # TODO: Load resource from ID
-        pass
+        self.__observation = Observation.objects.get(uuid=resource_id)
+        if not self.__observation:
+            raise ValueError('Resource does not exist.')
+        self.__concept = Concept.objects.get(uuid=__observation.concept)
+        if not self.__is_video_concept(self.__concept):
+            raise ValueError('Resource is not a video.')
+        self.__path = observation.value_complex.url()
+        self.__mimetype = self.__concept.mimetype
+
+    @class_method
+    def __is_video_concept(concept):
+        return concept.datatype == 'video'
 
     def get_info(self):
         return {
-            'path': '/static/sewi/video/video.mp4',
-            'type': 'video/webm'
+            'path': self.get_content(),
+            'type': self.get_type()
         }
 
     def get_content(self):
-        # TODO: return image data
-        pass
+        return self.__path;
 
     def get_type(self):
-        return 'video/webm'
+        return self.__mimetype
 
     def generate_thumbnail(self):
+        # TODO: Generate a thumbnail
         return '/'
