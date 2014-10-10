67afda04e89434ddcfd6fa8f47bbf8db5a973031
TheFuzzy <Infestor51@gmail.com>
2014-10-10 20:14:23 +0800

    Added helper event methods for ConfiguratorElement

--- static/sewi/js/app/configuratorElement.js
+++ static/sewi/js/app/configuratorElement.js
@@ -6,6 +6,26 @@ sewi.ConfiguratorElement = function() {
 	selfRef.mainDOMElement = $("<div></div>");
 }
 
+/**
+ * Retrieves the DOM element owned by this object.
+ * @return {jQuery} The DOM element container.
+ */
 sewi.ConfiguratorElement.prototype.getDOM = function() {
 	return this.mainDOMElement;
+}
+
+/**
+ * Helper function for triggering an event on the main DOM element.
+ * Arguments are exactly like jQuery.fn.trigger()
+ */
+sewi.ConfiguratorElement.prototype.trigger = function() {
+	this.mainDOMElement.trigger.apply(this.mainDOMElement, arguments);
+}
+
+/**
+* Helper function for triggering an event on the main DOM element.
+* Arguments are exactly like jQuery.fn.on()
+*/
+sewi.ConfiguratorElement.prototype.on = function() {
+	this.mainDOMElement.on.apply(this.mainDOMElement, arguments);
 }
