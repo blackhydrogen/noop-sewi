f94b04cfbca372f1f2f6546dc939ac40125abeba
TheFuzzy <Infestor51@gmail.com>
2014-10-11 16:25:22 +0800

    ResourceViewer button panel will now be hidden in fullscreen mode

--- static/sewi/js/app/common.js
+++ static/sewi/js/app/common.js
@@ -21,7 +21,7 @@ sewi.constants = {
     RESOURCE_VIEWER_CLOSE_BUTTON_DOM: '<button type="button" class="btn btn-danger close-button"><span aria-hidden="true">&times;</span></button>',
     RESOURCE_VIEWER_FULLSCREEN_BUTTON_DOM: '<button type="button" class="btn btn-default fullscreen-button">F</button>',
     RESOURCE_VIEWER_MOVE_BUTTON_DOM: '<button type="button" class="btn btn-default move-button">M</button>',
-    RESOURCE_VIEWER_PANEL_DOM: '<div class="top-panel"></div>',
+    RESOURCE_VIEWER_PANEL_DOM: '<div class="top-panel fullscreen-hidden"></div>',
 
 	MAX_NUM_TABS: 5,
 	DROP_AREA_POSITIONS: {TOP: 0, BOTTOM: 1, LEFT: 2, RIGHT: 3},
