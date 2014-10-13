2a0578fbd5968eb798801de3062d14300cd10794
Le Beier <moxlotus@live.com>
2014-10-13 08:37:34 +0800

    Add audio resource viewer

---
+++ static/sewi/js/app/audioResourceViewer.js
@@ -0,0 +1,11 @@
+var sewi = sewi || {};
+
+// Audio Resource Viewer Class
+sewi.AudioResourceViewer = function(){
+	var selfRef = this;
+
+}
+
+sewi.AudioResourceViewer.prototype.getDOM = function(){
+
+}
