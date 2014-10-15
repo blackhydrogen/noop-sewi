dceaf70de74e8670b645c26eb8dff057f71ba865
TheFuzzy <Infestor51@gmail.com>
2014-10-15 22:27:50 +0800

    ResourceViewers now use proper icons for the top button panels

--- static/sewi/js/app/common.js
+++ static/sewi/js/app/common.js
@@ -18,8 +18,8 @@ sewi.inherits = function(subClass, superClass) {
 /** Declare all constants in this object. */
 sewi.constants = {
     RESOURCE_VIEWER_CLASS: 'resource-viewer',
-    RESOURCE_VIEWER_CLOSE_BUTTON_DOM: '<button type="button" class="btn btn-danger close-button"><span aria-hidden="true">&times;</span></button>',
-    RESOURCE_VIEWER_FULLSCREEN_BUTTON_DOM: '<button type="button" class="btn btn-default fullscreen-button">F</button>',
+    RESOURCE_VIEWER_CLOSE_BUTTON_DOM: '<button type="button" class="btn btn-danger close-button"><span aria-hidden="true" class="glyphicon glyphicon-remove"></span></button>',
+    RESOURCE_VIEWER_FULLSCREEN_BUTTON_DOM: '<button type="button" class="btn btn-default fullscreen-button"><span aria-hidden="true" class="glyphicon glyphicon-fullscreen"></span></button>',
     RESOURCE_VIEWER_PANEL_DOM: '<div class="top-panel fullscreen-hidden"></div>',
 
 	MAX_NUM_TABS: 5,
