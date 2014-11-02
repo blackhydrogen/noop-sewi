b9470253baa1f4c61d69f8c63a75584f5ece346d
TheFuzzy <Infestor51@gmail.com>
2014-11-02 13:33:57 +0800

    Fixed errors preventing the main page from rendering

--- resource/__init__.py
+++ resource/__init__.py
@@ -2,4 +2,3 @@ from base_resource import BaseResource
 from image_resource import ImageResource
 from video_resource import VideoResource
 from audio_resource import AudioResource
-from basic_encounter_information import BasicEncounterInformation
