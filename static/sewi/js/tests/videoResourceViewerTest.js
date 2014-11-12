da8feca124f7371956a0b474f82a104ca8539f3b
TheFuzzy <Infestor51@gmail.com>
2014-11-12 21:12:50 +0800

    Added MediaControls unit tests for updating volume, position and playback state

--- static/sewi/js/tests/videoResourceViewerTest.js
+++ static/sewi/js/tests/videoResourceViewerTest.js
@@ -11,7 +11,16 @@
         TEST_INVALID_TIME_2: -1,
         TEST_VALID_TIME: 1,
         TEST_VALID_UPDATED_DURATION_TEXT: '00:01/02:36',
-        TEST_VOLUME: 0.5,
+        TEST_INVALID_VOLUME_1: 'silent',
+        TEST_INVALID_VOLUME_2: 1.1,
+        TEST_VALID_VOLUME: 0.5,
+        TEST_INVALID_UPDATED_VOLUME: 1.0,
+        TEST_VALID_UPDATED_VOLUME: 0.5,
+        TEST_INVALID_POSITION_1: 'silent',
+        TEST_INVALID_POSITION_2: 101,
+        TEST_VALID_POSITION: 45,
+        TEST_INVALID_UPDATED_POSITION: 100,
+        TEST_VALID_UPDATED_POSITION: 45.0,
 
         PLAY_BUTTON_CLASS: 'play-button',
         MUTE_BUTTON_CLASS: 'mute-button',
@@ -157,7 +166,7 @@
         var muteButton = this.fixture.find('.' + constants.MUTE_BUTTON_CLASS);
 
         controlsElement.one(constants.VOLUME_CHANGED_EVENT, function(event, volume) {
-            assert.equal(volumeSlider.val(), constants.TEST_VOLUME, 'Controls can adjust volume.');
+            assert.equal(volumeSlider.val(), constants.TEST_VALID_VOLUME, 'Controls can adjust volume.');
             QUnit.start();
         });
 
@@ -167,7 +176,7 @@
         });
 
         muteButton.click();
-        volumeSlider.val(constants.TEST_VOLUME).trigger('input').trigger('change');
+        volumeSlider.val(constants.TEST_VALID_VOLUME).trigger('input').trigger('change');
     });
 
     QUnit.test('Updating Time and Duration', function(assert) {
@@ -207,5 +216,78 @@
             currentTime: constants.TEST_VALID_TIME,
         });
         assert.equal(durationDisplay.text(), constants.TEST_VALID_UPDATED_DURATION_TEXT, 'Current time updates when non-negative number is provided.');
+    });
+
+    QUnit.test('Updating Volume', function(assert) {
+        var controls = new this.sewi.MediaControls();
+        this.fixture.append(controls.getDOM());
+        var controlsElement = controls.getDOM();
+
+        var volumeSlider = this.fixture.find('.' + constants.VOLUME_SLIDER_CLASS);
+        var muteButton = this.fixture.find('.' + constants.MUTE_BUTTON_CLASS);
+        var initialVolume = volumeSlider.val();
+
+        controls.update({
+            volume: constants.TEST_INVALID_VOLUME_1,
+        });
+        assert.equal(volumeSlider.val(), initialVolume, 'Volume does not update when non-number is provided.');
+
+        controls.update({
+            volume: constants.TEST_INVALID_VOLUME_2,
+        });
+        assert.equal(volumeSlider.val(), constants.TEST_INVALID_UPDATED_VOLUME, 'Volume defaults to 1 when number is over the valid range.');
+
+        controls.update({
+            volume: constants.TEST_VALID_VOLUME,
+        });
+        assert.equal(volumeSlider.val(), constants.TEST_VALID_UPDATED_VOLUME, 'Volume updates when number between 0 and 1 is provided.');
+
+        muteButton.click();
+
+        controls.update({
+            volume: constants.TEST_VALID_VOLUME,
+        });
+
+        assert.ok(!controlsElement.hasClass(constants.MUTED_CLASS), 'Controls are unmuted when volume is updated.');
+    });
+
+    QUnit.test('Updating Playback Position', function(assert) {
+        var controls = new this.sewi.MediaControls();
+        this.fixture.append(controls.getDOM());
+        var controlsElement = controls.getDOM();
+
+        var progressSlider = this.fixture.find('.' + constants.PROGRESS_SLIDER_CLASS);
+        var initialPosition = progressSlider.val();
+
+        controls.update({
+            position: constants.TEST_INVALID_POSITION_1,
+        });
+        assert.equal(progressSlider.val(), initialPosition, 'Position does not update when non-number is provided.');
+
+        controls.update({
+            position: constants.TEST_INVALID_POSITION_2,
+        });
+        assert.equal(progressSlider.val(), constants.TEST_INVALID_UPDATED_POSITION, 'Position defaults to 1 when number is over the valid range.');
+
+        controls.update({
+            position: constants.TEST_VALID_POSITION,
+        });
+        assert.equal(progressSlider.val(), constants.TEST_VALID_UPDATED_POSITION, 'Position updates when number between 0 and 1 is provided.');
+    });
+
+    QUnit.test('Updating Playback State', function(assert) {
+        var controls = new this.sewi.MediaControls();
+        this.fixture.append(controls.getDOM());
+        var controlsElement = controls.getDOM();
+
+        controls.update({
+            playing: true,
+        });
+        assert.ok(controlsElement.hasClass(constants.PLAYING_CLASS), 'Controls can emulate playing mode.');
+
+        controls.update({
+            playing: false,
+        });
+        assert.ok(!controlsElement.hasClass(constants.PLAYING_CLASS), 'Controls can emulate paused mode.');
     });
 })();
